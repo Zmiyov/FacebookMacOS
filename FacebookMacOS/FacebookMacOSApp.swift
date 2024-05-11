87fda9936c58d3d81a987ae5169f4a1d3e4c6fdc
Volodymyr <zmiyov88@gmail.com>
2024-05-11 18:33:37 +0300

    Initial Commit

---
+++ FacebookMacOS/FacebookMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookMacOSApp.swift
+//  FacebookMacOS
+//
+//  Created by Vladimir Pisarenko on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FacebookMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
